5bb41b54c475fc71dde6b7abf7aa5df5c3fe6305
Ser0zha <serzh.krivov.05@mail.ru>
2024-02-04 00:01:02 +0300

    first sort 1.1

---
+++ main.py
@@ -0,0 +1,17 @@
+class Solution:
+    def soortbeta(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        while True:
+            cnt = 0
+            for i in range(n - 1):
+                if nums[i] > nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                    cnt += 1
+            if not cnt:
+                break
+        return nums
+
+
+k = [int(i) for i in input().split()]
+sol = Solution()
+print(*sol.soortbeta(k))
