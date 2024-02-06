245a601ed98b246d7e94cb201b64371678fecbd6
Ser0zha <137228126+Ser0zha@users.noreply.github.com>
2024-02-06 22:02:18 +0300

    Update main.py
     + (-cnt)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class Solution:
         n = len(nums)
         while True:
             cnt = 0
-            for i in range(n - 1):
+            for i in range(n - 1 - cnt):
                 if nums[i] > nums[i + 1]:
                     nums[i], nums[i + 1] = nums[i + 1], nums[i]
                     cnt += 1
