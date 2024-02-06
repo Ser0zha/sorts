508af8d975f4a6714590ca50ff8aca0d354bd989
Ser0zha <137228126+Ser0zha@users.noreply.github.com>
2024-02-06 22:04:50 +0300

    +k

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 class Solution:
     def soortbeta(self, nums: list[int]) -> list[int]:
         n = len(nums)
+        k = 0
         while True:
             cnt = 0
-            for i in range(n - 1 - cnt):
+            for i in range(n - 1 - k):
                 if nums[i] > nums[i + 1]:
                     nums[i], nums[i + 1] = nums[i + 1], nums[i]
                     cnt += 1
+            k += 1
             if not cnt:
                 break
         return nums
